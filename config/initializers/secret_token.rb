a28b7a68b129325f9a01e9a78f74131877f004b6
Dave Horn <dave.horn@plansource.com>
2015-05-08 13:50:55 -0700

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '1043454931816463001a12ea21f8b80e7c903448dd0aa5fb718a3dc4c2b7e44ceb51f147e7b1322b04c5e7cf194b7d4e83f33b3c3b0cfea34dc89b224e493209'
